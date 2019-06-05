20978e936567bb4bfdccbbd69b2b61624eb8b4fc
Jakub <49204020+Panik1997@users.noreply.github.com>
2019-06-05 19:36:16 +0200

    Create University.java

---
+++ University.java
@@ -0,0 +1,11 @@
+public class University {
+    private String direction;
+
+    public University(String direction) {
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+}
